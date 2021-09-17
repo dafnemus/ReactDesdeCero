0ad30def5bb5de046513e6b12fdea27c0cc13657
dafnemus <dafnemus@gmail.com>
2021-09-17 12:44:11 -0300

    mensaje de logueado exitoso

--- login-udemy/src/ejercicios/BasicLogin/index.js
+++ login-udemy/src/ejercicios/BasicLogin/index.js
@@ -5,6 +5,7 @@ function LoginFormBasic() {
   const [form, setForm] = useState({
     email: '',
     password: '',
+    isLogin: false,
   });
 
   const handleInputChange = ({ target: { id, value } }) =>
@@ -12,28 +13,34 @@ function LoginFormBasic() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    alert(`Bienvenide ${form.email}`);
+    setForm((last) => ({ ...last, isLogin: true }));
   };
 
   return (
-    <form className='form-container' onSubmit={handleSubmit}>
-      <h2>Iniciar Sesión</h2>
-      <label>Email:</label>
-      <input
-        id='email'
-        type='email'
-        value={form.email}
-        onChange={handleInputChange}
-      />
-      <label>Contraseña:</label>
-      <input
-        id='password'
-        type='password'
-        value={form.password}
-        onChange={handleInputChange}
-      />
-      <button type='submit'>Enviar</button>
-    </form>
+    <>
+      {form.isLogin ? (
+        <h2 className='title'> Bienvenido {`${form.email}`}!!!</h2>
+      ) : (
+        <form className='form-container' onSubmit={handleSubmit}>
+          <h2>Iniciar Sesión</h2>
+          <label>Email:</label>
+          <input
+            id='email'
+            type='email'
+            value={form.email}
+            onChange={handleInputChange}
+          />
+          <label>Contraseña:</label>
+          <input
+            id='password'
+            type='password'
+            value={form.password}
+            onChange={handleInputChange}
+          />
+          <button type='submit'>Enviar</button>
+        </form>
+      )}
+    </>
   );
 }
 
