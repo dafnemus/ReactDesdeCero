fd416f68ee9a6f5aff252d950e7fc85aa6567485
dafnemus <dafnemus@gmail.com>
2022-10-11 11:39:18 -0300

    created hanleCloseOpenSearchClick

--- login-udemy/src/ejercicios/search/index.js
+++ login-udemy/src/ejercicios/search/index.js
@@ -1,21 +1,18 @@
-import { useState } from "react";
-import SearchBox from "./components/SearchBox";
-import "./style.css";
+import { useState } from 'react';
+import SearchBox from './components/SearchBox';
+import './style.css';
 
 export default function Search() {
   const [isAtop, setIsAtTop] = useState(false);
 
-  const handleSearchClick = () => {
-    setIsAtTop(true);
-  };
+  const handleCloseOpenSearchClick = () => setIsAtTop(!isAtop);
 
-  const handleCloseClick = () => {
-    setIsAtTop(false);
-  };
-  
   return (
-    <div className={`search ${isAtop ? "searchTop" : "searchCenter"}`}>
-      <SearchBox onSearch={handleSearchClick} onClose={handleCloseClick} />
+    <div className={`search ${isAtop ? 'searchTop' : 'searchCenter'}`}>
+      <SearchBox
+        onSearch={handleCloseOpenSearchClick}
+        onClose={handleCloseOpenSearchClick}
+      />
     </div>
   );
 }
