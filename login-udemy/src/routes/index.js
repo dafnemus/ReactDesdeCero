03ff5cc0cd0a073a63b18939bc078c53a8a3b3aa
dafnemus <dafnemus@gmail.com>
2022-10-14 10:58:40 -0300

    added NotFound view and route

--- login-udemy/src/routes/index.js
+++ login-udemy/src/routes/index.js
@@ -1,16 +1,22 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from '../views/Login';
 import SearchStaff from '../views/SearchStaff';
+import NotFound from '../views/NotFound';
 
 export default function Routes() {
   return (
     <Router>
-      <Route path="/" exact>
-        <Login />
-      </Route>
-      <Route path="/search" exact>
-        <SearchStaff />
-      </Route>
+      <Switch>
+        <Route path="/" exact>
+          <Login />
+        </Route>
+        <Route path="/search" exact>
+          <SearchStaff />
+        </Route>
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
     </Router>
   );
 }
---
+++ login-udemy/src/views/NotFound.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <>
+      <h1 className="title">404</h1>
+      <p>Not Found</p>
+    </>
+  );
+}
